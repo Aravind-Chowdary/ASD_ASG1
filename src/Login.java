d6ec8e50655ea702e56139de91634abc62531a86
Aravind <aravindmullapudi@gmail.com>
2020-04-04 01:53:38 +0100

    Building the Jframe

--- src/Login.java
+++ src/Login.java
@@ -1,2 +1,26 @@
-public class Login {
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+class Login extends JFrame implements ActionListener {
+    JFrame jf;
+    Font f;
+    Login()
+    {
+        jf=new JFrame();
+        f = new Font("Times New Roman",Font.BOLD,20);
+        jf.setLayout(null);
+    }
+    public void actionPerformed(ActionEvent ae)
+    {
+
+    }
+
+    public static void main(String args[])
+    {
+        new Login();
+
+    }
 }
