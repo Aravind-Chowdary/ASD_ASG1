94bf36cc98b11dd37ef65edd6b50dda88d930ac5
Aravind <aravindmullapudi@gmail.com>
2020-04-04 02:51:45 +0100

    Adding Labels and Text fields for login page

--- src/Login.java
+++ src/Login.java
@@ -6,7 +6,8 @@ import javax.swing.*;
 
 class Login extends JFrame implements ActionListener {
     JFrame jf;
-    JLabel l5;
+    JLabel l5,l1;
+    JTextField t1;
     Font f;
     Login()
     {
@@ -16,6 +17,15 @@ class Login extends JFrame implements ActionListener {
         l5 = new JLabel("Login Here");l5.setFont(new Font("Times New Roman",Font.BOLD,30));
         l5.setBounds(300,100,300,40);
         jf.add(l5);
+
+        l1 = new JLabel("User Name : "); l1.setFont(f);
+        l1.setBounds(200,250,200,25);
+        jf.add(l1);
+
+        t1 = new JTextField(20);
+        t1.setBounds(350,250,200,25);
+        t1.setToolTipText("Enter Username");
+        jf.add(t1);
     }
     public void actionPerformed(ActionEvent ae)
     {
