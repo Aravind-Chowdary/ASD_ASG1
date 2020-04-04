df98dc02c964413742e470fb69327c1050c50589
Aravind <aravindmullapudi@gmail.com>
2020-04-04 02:48:43 +0100

    Adding Labels

--- src/Login.java
+++ src/Login.java
@@ -6,12 +6,16 @@ import javax.swing.*;
 
 class Login extends JFrame implements ActionListener {
     JFrame jf;
+    JLabel l5;
     Font f;
     Login()
     {
         jf=new JFrame();
         f = new Font("Times New Roman",Font.BOLD,20);
         jf.setLayout(null);
+        l5 = new JLabel("Login Here");l5.setFont(new Font("Times New Roman",Font.BOLD,30));
+        l5.setBounds(300,100,300,40);
+        jf.add(l5);
     }
     public void actionPerformed(ActionEvent ae)
     {
