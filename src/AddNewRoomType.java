f17ce7f1b6e180978955458b12b158aab5298f7a
Aravind <aravindmullapudi@gmail.com>
2020-04-15 16:33:36 +0100

    Adding fields in page

--- src/AddNewRoomType.java
+++ src/AddNewRoomType.java
@@ -5,10 +5,23 @@ import java.awt.event.*;
 public class AddNewRoomType extends JFrame implements ActionListener
 {
     JFrame jf;
+    JLabel l2;
+    JTextField t2;
     AddNewRoomType()
     {
         jf=new JFrame();
         jf.setLayout(null);
+
+        l2 = new JLabel("Enter Room Type Name*");
+        //l2.setFont(f);
+        l2.setBounds(150,160,170,25);
+        jf.add(l2);
+
+        t2=new JTextField(20);
+        t2.setBounds(300,160,200,25);t2.setToolTipText("Enter Room Type Name");
+        jf.add(t2);
+
+
         jf.setTitle("Add New Room Type");
         jf.setLocation(20,20);
         jf.setResizable(false);
