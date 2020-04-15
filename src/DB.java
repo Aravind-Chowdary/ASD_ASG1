b6e95af23bc24ab3e6813ed3c71781f148b53749
Aravind <aravindmullapudi@gmail.com>
2020-04-15 13:06:09 +0100

    Successful connection to the database

--- src/DB.java
+++ src/DB.java
@@ -8,7 +8,11 @@ public class DB {
         Connection con=null;
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            con=DriverManager.getConnection("https://selene.hud.ac.uk/phpmyadmin/db_structure.php?server=1&db=u1950905&token=724ba7cd7b938cd42594c6fc7c180759","U1950905","AK06mar19hj");
+            con=DriverManager.getConnection("jdbc:mysql://selene.hud.ac.uk/u1950905","u1950905","AK06mar19hj");
+            if (con != null) {
+                System.out.println("yes");
+            }
+
         }catch(Exception e){System.out.println(e);}
         return con;
     }
