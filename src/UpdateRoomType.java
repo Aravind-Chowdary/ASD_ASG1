968b06cca2aa67387b28f52b5e37f6a03ce54f03
Aravind <aravindmullapudi@gmail.com>
2020-04-17 23:56:05 +0100

    Implementing the UpdateRoomType class

--- src/UpdateRoomType.java
+++ src/UpdateRoomType.java
@@ -1,3 +1,11 @@
-public class UpdateRoomType {
-    //This class is run the process if they select the update room item in main Menu page
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UpdateRoomType extends JFrame implements ActionListener
+{
+    public void actionPerformed(ActionEvent ae)
+    {
+        
+    }
 }
