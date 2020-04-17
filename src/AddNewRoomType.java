6797f337bf6ad29d696fc55a5d94f895bb55f8b3
Aravind <aravindmullapudi@gmail.com>
2020-04-17 23:51:52 +0100

    Added exception

--- src/AddNewRoomType.java
+++ src/AddNewRoomType.java
@@ -118,6 +118,11 @@ public class AddNewRoomType extends JFrame implements ActionListener
                 System.out.println(se);
                 JOptionPane.showMessageDialog(null,"SQL Error:"+se);
             }
+            catch(Exception e)
+            {
+                System.out.println(e);
+                JOptionPane.showMessageDialog(null,"Error:"+e);
+            }
         }
     }
     public static void main(String args[])
