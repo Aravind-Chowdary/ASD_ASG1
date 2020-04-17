3349a1e09cadb4e4fedeb4a4e885578e2a979bd1
Aravind <aravindmullapudi@gmail.com>
2020-04-17 23:59:17 +0100

    Setting up the frame

--- src/UpdateRoomType.java
+++ src/UpdateRoomType.java
@@ -1,11 +1,32 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class UpdateRoomType extends JFrame implements ActionListener
 {
+    JFrame jf;
+    UpdateRoomType()
+    {
+        jf=new JFrame();
+        Dimension screen= Toolkit.getDefaultToolkit().getScreenSize();
+        jf.setLayout(null);
+
+        jf.setTitle("Update Zone");
+        // jf.setSize(900,700);
+        jf.setLocation(20,20);
+        jf.setResizable(false);
+        jf.getContentPane().setBackground(Color.cyan);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        jf.setBounds(0,0,screenSize.width, screenSize.height-50);
+        jf.setVisible(true);
+    }
     public void actionPerformed(ActionEvent ae)
     {
-        
+
+    }
+    public static void main(String args[])
+    {
+        new UpdateRoomType();
     }
 }
