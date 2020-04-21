a85b9becc35eb8a63d0f1cb1ccbb282ecec3047d
Aravind <aravindmullapudi@gmail.com>
2020-04-21 23:26:12 +0100

    AWT event

--- src/AddNewRoom.java
+++ src/AddNewRoom.java
@@ -1,3 +1,13 @@
-public class AddNewRoom {
-    //This class is implemented to handle the process to add new room
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+public class AddNewRoom extends JFrame implements ActionListener
+{
+
+
+
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
