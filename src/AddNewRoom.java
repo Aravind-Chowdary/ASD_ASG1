f133662eafaa79697b9fba29011cf40fd47aeccb
Aravind <aravindmullapudi@gmail.com>
2020-04-21 23:35:57 +0100

    Setting up the required fields with appropriate labels and text fields

--- src/AddNewRoom.java
+++ src/AddNewRoom.java
@@ -6,10 +6,20 @@ public class AddNewRoom extends JFrame implements ActionListener
 {
     JFrame jf;
     Font f;
+    JLabel l2;
+    JTextField t2;
     AddNewRoom(){
         jf=new JFrame();
         f = new Font("Times New Roman",Font.BOLD,20);
         jf.setLayout(null);
+
+        l2 = new JLabel("Room name*");
+        l2.setBounds(150,160,170,25);
+        jf.add(l2);
+
+        t2=new JTextField(20);
+        t2.setBounds(320,160,250,25);t2.setToolTipText("Enter Room name");
+        jf.add(t2);
 
         jf.setTitle("Add New Room");
         //jf.setSize(900,700);
@@ -25,4 +35,10 @@ public class AddNewRoom extends JFrame implements ActionListener
     public void actionPerformed(ActionEvent e) {
 
     }
+    public static void main(String args[])
+    {
+        new AddNewRoom();
+    }
+
+
 }
