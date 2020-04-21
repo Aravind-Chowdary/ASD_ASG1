328a5a4826630cc4001230d9f01ae78926005ec5
Aravind <aravindmullapudi@gmail.com>
2020-04-21 23:31:42 +0100

    Design the frame

--- src/AddNewRoom.java
+++ src/AddNewRoom.java
@@ -1,11 +1,26 @@
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 public class AddNewRoom extends JFrame implements ActionListener
 {
+    JFrame jf;
+    Font f;
+    AddNewRoom(){
+        jf=new JFrame();
+        f = new Font("Times New Roman",Font.BOLD,20);
+        jf.setLayout(null);
 
+        jf.setTitle("Add New Room");
+        //jf.setSize(900,700);
+        jf.setLocation(20,20);
+        jf.setResizable(false);
+        jf.getContentPane().setBackground(Color.cyan);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        jf.setBounds(0,0,screenSize.width, screenSize.height-50);
+        jf.setVisible(true);
 
-
+                }
 
     public void actionPerformed(ActionEvent e) {
 
