37d64582e4e1b44a172abb3b97dcdb3085866f20
Aravind <aravindmullapudi@gmail.com>
2020-04-21 23:39:03 +0100

    Adding required fields

--- src/AddNewRoom.java
+++ src/AddNewRoom.java
@@ -6,8 +6,8 @@ public class AddNewRoom extends JFrame implements ActionListener
 {
     JFrame jf;
     Font f;
-    JLabel l2;
-    JTextField t2;
+    JLabel l2,l3;
+    JTextField t2,t3;
     AddNewRoom(){
         jf=new JFrame();
         f = new Font("Times New Roman",Font.BOLD,20);
@@ -18,8 +18,18 @@ public class AddNewRoom extends JFrame implements ActionListener
         jf.add(l2);
 
         t2=new JTextField(20);
-        t2.setBounds(320,160,250,25);t2.setToolTipText("Enter Room name");
+        t2.setBounds(320,160,250,25);
+        t2.setToolTipText("Enter Room name");
         jf.add(t2);
+
+        l3 = new JLabel("Room Size*");
+        l3.setBounds(150,200,170,25);
+        jf.add(l3);
+
+        t3=new JTextField(20);
+        t3.setBounds(320,200,250,25);
+        t3.setToolTipText("Enter Room Size");
+        jf.add(t3);
 
         jf.setTitle("Add New Room");
         //jf.setSize(900,700);
