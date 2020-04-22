9e02c8222fb2695585e62cf52c2b7d78a875fc00
Aravind <aravindmullapudi@gmail.com>
2020-04-22 23:34:18 +0100

    Setting the Mange room page

--- src/UpdateRoom.java
+++ src/UpdateRoom.java
@@ -1,3 +1,15 @@
-public class UpdateRoom {
-    //This Class is to build the update room page
+import javax.swing.*;
+import java.awt.event.*;
+
+public class UpdateRoom extends JFrame implements ActionListener
+{
+
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public static void main(String args[])
+    {
+        new UpdateRoom();
+    }
 }
