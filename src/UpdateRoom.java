fbb32e2d30c0d757660661870e292ad6825b7e69
Aravind <aravindmullapudi@gmail.com>
2020-04-22 23:45:25 +0100

    Setting up the frame

--- src/UpdateRoom.java
+++ src/UpdateRoom.java
@@ -1,9 +1,24 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.*;
 
 public class UpdateRoom extends JFrame implements ActionListener
 {
+    JFrame jf;
+    UpdateRoom()
+    {
+        jf = new JFrame();
+        jf.setLayout(null);
 
+        jf.setTitle("Update Room");
+        // jf.setSize(900,700);
+        jf.setLocation(20,20);
+        jf.setResizable(false);
+        jf.getContentPane().setBackground(Color.cyan);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        jf.setBounds(0,0,screenSize.width, screenSize.height-50);
+        jf.setVisible(true);
+    }
 
     public void actionPerformed(ActionEvent e) {
 
