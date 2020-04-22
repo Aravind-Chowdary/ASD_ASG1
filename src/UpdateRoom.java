107613e4416c4bfa737ed241f40fa7f856491e33
Aravind <aravindmullapudi@gmail.com>
2020-04-22 23:54:18 +0100

    Adding the required labels

--- src/UpdateRoom.java
+++ src/UpdateRoom.java
@@ -5,10 +5,21 @@ import java.awt.*;
 public class UpdateRoom extends JFrame implements ActionListener
 {
     JFrame jf;
+    JLabel l1;
+    JTextField t1;
     UpdateRoom()
     {
         jf = new JFrame();
         jf.setLayout(null);
+
+        l1= new JLabel("Room  id *");
+        //l1.setFont(f);
+        l1.setBounds(150,120,130,25);
+        jf.add(l1);
+
+        t1=new JTextField(20);
+        t1.setBounds(320,120,250,25);t1.setToolTipText("Enter Room id");
+        jf.add(t1);
 
         jf.setTitle("Update Room");
         // jf.setSize(900,700);
