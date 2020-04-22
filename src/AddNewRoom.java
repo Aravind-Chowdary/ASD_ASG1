b406b8dbd859f69e3d9b2f333ce2ca82255c79be
Aravind <aravindmullapudi@gmail.com>
2020-04-22 23:04:45 +0100

    Sql command for action

--- src/AddNewRoom.java
+++ src/AddNewRoom.java
@@ -189,6 +189,12 @@ public class AddNewRoom extends JFrame implements ActionListener
                 try {
                     con = db.getConnection();
                     System.out.println("Connected to database.");
+                    stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+                    rs = stmt.executeQuery("select * from room_manager " );
+                    while(rs.next())
+                    {
+                        model.insertRow(r++, new Object[]{rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7) });
+                    }
                     con.close();
                 } catch (SQLException se) {
                     System.out.println(se);
