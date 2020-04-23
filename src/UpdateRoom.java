57c6514afb6c78aca51b2ea28f01c3699a443ce4
Aravind <aravindmullapudi@gmail.com>
2020-04-23 17:36:30 +0100

    Clear button

--- src/UpdateRoom.java
+++ src/UpdateRoom.java
@@ -10,7 +10,7 @@ public class UpdateRoom extends JFrame implements ActionListener
     JFrame jf;
     JLabel l1,l2,l3,l4,l5,l6,l7,l8;
     JTextField t1,t2,t3,t4,t5,t7;
-    JButton b0,b1;
+    JButton b0,b1,b2;
     Font f;
     JComboBox c1;
     Connection con;
@@ -120,6 +120,10 @@ public class UpdateRoom extends JFrame implements ActionListener
         b1.setToolTipText("click to update room details");
         jf.add(b1);
 
+        b2 = new JButton("Clear");
+        b2.setBounds(450,390,110,35);
+        b2.setToolTipText("click to clear all");
+        jf.add(b2);
 
         jf.setTitle("Update Room");
         // jf.setSize(900,700);
