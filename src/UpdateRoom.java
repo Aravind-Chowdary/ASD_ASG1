f76b571bce703481479ed3c1f0ce3e664e5fee60
Aravind <aravindmullapudi@gmail.com>
2020-04-24 23:14:09 +0100

    Connection to the database

--- src/UpdateRoom.java
+++ src/UpdateRoom.java
@@ -184,7 +184,18 @@ public class UpdateRoom extends JFrame implements ActionListener
             {
                 JOptionPane.showMessageDialog(this,"* Detail are Missing !","Warning!!!",JOptionPane.ERROR_MESSAGE);
             }
-        }
+            else
+            {
+                try {
+                    con = db.getConnection();
+                    System.out.println("Connected to database.");
+                    con.close();
+                } catch(SQLException se)
+                {
+                    System.out.println(se);
+                    JOptionPane.showMessageDialog(null,"SQL Error:"+se);
+                }
+            }
     }
     public static void main(String args[])
     {
