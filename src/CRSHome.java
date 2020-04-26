026ae51dadb0c96a4c39a5d74a6f05afc8aa1288
Aravind <aravindmullapudi@gmail.com>
2020-04-26 23:00:03 +0100

    Setting up the Frame

--- src/CRSHome.java
+++ src/CRSHome.java
@@ -1,5 +1,24 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class CRSHome extends JFrame {
+    static CRSHome frame;
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    frame= new CRSHome();
+                    frame.setResizable(false);
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
+
+    public CRSHome() {
+
+    }
 }
