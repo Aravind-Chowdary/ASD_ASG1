634ad325803f96bdc9fef4f69df0661e53022848
Aravind <aravindmullapudi@gmail.com>
2020-04-26 23:28:23 +0100

    Contents pane

--- src/CRSHome.java
+++ src/CRSHome.java
@@ -1,8 +1,11 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class CRSHome extends JFrame {
     static CRSHome frame;
+    private JPanel contentPane;
+    DB db = null;
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -23,6 +26,13 @@ public class CRSHome extends JFrame {
 
 
     public CRSHome() {
+        db = new DB();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 500, 500);
+        contentPane = new JPanel();
 
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setBackground(Color.cyan);
+        setContentPane(contentPane);
     }
 }
