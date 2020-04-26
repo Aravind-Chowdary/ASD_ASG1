b1b3c88e9be7b4ca0846bfbb9f318d9f3861cbbc
Aravind <aravindmullapudi@gmail.com>
2020-04-26 23:06:20 +0100

    Bounds for the frame

--- src/CRSHome.java
+++ src/CRSHome.java
@@ -8,6 +8,10 @@ public class CRSHome extends JFrame {
             public void run() {
                 try {
                     frame= new CRSHome();
+                    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+                    frame.setBounds(0,0,screenSize.width, screenSize.height);
+
+                    frame.getContentPane().setLayout(new GridBagLayout());
                     frame.setResizable(false);
                     frame.setVisible(true);
                 } catch (Exception e) {
