e390077f71c59da7dcb319fe0a692dbf00b6a555
Aravind <aravindmullapudi@gmail.com>
2020-04-29 23:17:36 +0100

    Scrl pane to view the bookings

--- src/AssignList.java
+++ src/AssignList.java
@@ -1,13 +1,37 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.sql.Connection;
 
 public class AssignList extends JFrame
 {
     JFrame jf;
+    DB db = null;
+    JLabel ln;
+    Connection con;
+    DefaultTableModel model = new DefaultTableModel();
+    JTable tabGrid = new JTable(model);
+    JScrollPane scrlPane = new JScrollPane(tabGrid);
     public AssignList(){
+        db = new DB();
         jf = new JFrame();
         jf.setLayout(null);
 
+        ln = new JLabel("List Of Availble Room Details");
+        ln.setFont(new Font("Times New Roman",Font.BOLD,25));
+        ln.setForeground(Color.blue);
+        ln.setBounds(300,30,350,25);
+        jf.add(ln);
+
+        scrlPane.setBounds(80,80,900,600);
+        jf.add(scrlPane);
+        tabGrid.setFont(new Font ("Times New Roman",0,15));
+
+        model.addColumn("A_ID");
+        model.addColumn("Room");
+        model.addColumn("Date");
+        model.addColumn("Type");
+        model.addColumn("Time");
 
         jf.setTitle("List of Available Room Details");
         jf.setLocation(20,20);
