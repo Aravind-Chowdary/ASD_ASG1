133e4e767a98367bd8e64a4fa0f5a90131152bea
Aravind <aravindmullapudi@gmail.com>
2020-04-30 00:07:46 +0100

    labels and text fields

--- src/UpdateRoomAvailability.java
+++ src/UpdateRoomAvailability.java
@@ -6,9 +6,21 @@ import java.awt.event.ActionListener;
 public class UpdateRoomAvailability extends JFrame implements ActionListener
 {
     JFrame jf;
+    JLabel l1;
+    JTextField t1;
     UpdateRoomAvailability(){
         jf = new JFrame();
         jf.setLayout(null);
+
+        l1 = new JLabel("Assign Id *");
+        l1.setBounds(150,90,130,25);
+        jf.add(l1);
+
+        t1=new JTextField(20);
+        t1.setBounds(320,90,200,25);
+        t1.setToolTipText("Id");
+        jf.add(t1);
+
 
         jf.setTitle("Manage Bookings ");
         jf.setLocation(20,20);
