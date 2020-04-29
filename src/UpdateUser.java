4084873b47968a902c32a599c387a63fc472e6d0
Aravind <aravindmullapudi@gmail.com>
2020-04-29 14:28:25 +0100

    Managing users main class

--- src/UpdateUser.java
+++ src/UpdateUser.java
@@ -1,2 +1,16 @@
-public class UpdateUser {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UpdateUser extends JFrame implements ActionListener
+{
+
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public static void main(String args[])
+    {
+        new UpdateUser();
+    }
 }
