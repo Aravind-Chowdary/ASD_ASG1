20d7bdd6768476e1bb271580eff41a0809f1725a
Aravind <aravindmullapudi@gmail.com>
2020-04-29 12:01:33 +0100

    Implementation of the Adduser class

--- src/AddUser.java
+++ src/AddUser.java
@@ -1,2 +1,16 @@
-public class AddUser {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AddUser extends JFrame implements ActionListener
+{
+
+   
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public static void main(String args[])
+    {
+        new AddUser();
+    }
 }
