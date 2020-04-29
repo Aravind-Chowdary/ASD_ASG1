0ac1b58f461ece071003c073b6128994fdf8793e
Aravind <aravindmullapudi@gmail.com>
2020-04-29 15:32:38 +0100

    Setting up the Jframe

--- src/UpdateUser.java
+++ src/UpdateUser.java
@@ -1,10 +1,24 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class UpdateUser extends JFrame implements ActionListener
 {
+    JFrame jf;
+    UpdateUser(){
+        jf = new JFrame();
+        jf.setLayout(null);
 
+        jf.setTitle("Update Users");
+        // jf.setSize(900,700);
+        jf.setLocation(20,20);
+        jf.setResizable(false);
+        jf.getContentPane().setBackground(Color.cyan);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        jf.setBounds(0,0,screenSize.width, screenSize.height-50);
+        jf.setVisible(true);
+    }
 
     public void actionPerformed(ActionEvent e) {
 
