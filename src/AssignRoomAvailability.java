309ca1d4df20efbce4c6ef855814f6b6757b2e87
Aravind <aravindmullapudi@gmail.com>
2020-04-29 21:16:33 +0100

    Drop down menu to select the available rooms

--- src/AssignRoomAvailability.java
+++ src/AssignRoomAvailability.java
@@ -2,13 +2,46 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class AssignRoomAvailability extends JFrame implements ActionListener
 {
     JFrame jf;
+    JLabel l1;
+    JComboBox c1;
+    DB db =null;
+    Connection con;
     AssignRoomAvailability(){
         jf = new JFrame();
         jf.setLayout(null);
+
+        l1 = new JLabel("Select Room *");
+        l1.setBounds(150,120,130,25);
+        jf.add(l1);
+
+        c1=new JComboBox();
+        c1.setBounds(320,120,200,25);c1.setToolTipText("Select Room");
+        jf.add(c1);
+        c1.addItem("Select Room");
+
+        try {
+            con=db.getConnection();
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("Select rtitle from room_manager");
+            while (rs.next()) {
+                String round = rs.getString("rtitle");
+                c1.addItem(round);
+            }
+            rs.close();
+            con.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
         jf.setTitle("Assign Day and Room Availability");
         jf.setLocation(20,20);
         jf.setResizable(false);
