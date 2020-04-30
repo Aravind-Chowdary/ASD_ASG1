9b708ee9cbd0ff44b247d45fcbd8fe0970682ecd
Aravind <aravindmullapudi@gmail.com>
2020-04-30 03:11:27 +0100

    Minor changes

--- src/AssignList.java
+++ src/AssignList.java
@@ -44,7 +44,7 @@ public class AssignList extends JFrame
             con=db.getConnection();
             System.out.println("Connected to database.");
             stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
-            rs = stmt.executeQuery("select * from room_availbility " );
+            rs = stmt.executeQuery("select * from room_availbility" );
             while(rs.next())
             {
                 model.insertRow(r++, new Object[]{rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5) });
