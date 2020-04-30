48e18bbd502f9ed0a379c88199dc6e7126b189d5
Aravind <aravindmullapudi@gmail.com>
2020-04-30 18:22:12 +0100

    Building the Clerk Login page

---
+++ src/LoginClerk.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+
+class LoginClerk extends JFrame implements ActionListener
+{
+
+    public void actionPerformed(ActionEvent ae)
+    {
+
+    }
+
+    public static void main(String args[])
+    {
+        new LoginClerk();
+
+    }
+
+}
