63fe9c93b87c3ef740537e982464338b599ed842
Aravind <aravindmullapudi@gmail.com>
2020-04-30 18:26:51 +0100

    Duplicate to the Login page!!

--- src/LoginClerk.java
+++ src/LoginClerk.java
@@ -1,9 +1,22 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
 
 class LoginClerk extends JFrame implements ActionListener
 {
+    JFrame jf;
+    LoginClerk(){
+        jf=new JFrame();
+        jf.setLayout(null);
+
+        jf.setTitle("Login");
+        jf.setLocation(20,20);
+        jf.setSize(800,600);
+        jf.setResizable(false);
+        jf.getContentPane().setBackground(Color.cyan);
+        jf.setVisible(true);
+    }
 
     public void actionPerformed(ActionEvent ae)
     {
