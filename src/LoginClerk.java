5d409c3511cf1b97073fa36bc421ecf8048982ab
Aravind <aravindmullapudi@gmail.com>
2020-04-30 18:35:43 +0100

    Frame design

--- src/LoginClerk.java
+++ src/LoginClerk.java
@@ -6,9 +6,40 @@ import java.awt.event.*;
 class LoginClerk extends JFrame implements ActionListener
 {
     JFrame jf;
+    JLabel l1,l2,l5;
+    JTextField t1;
+    JPasswordField p1;
+    Font f;
+
     LoginClerk(){
         jf=new JFrame();
+        f = new Font("Times New Roman",Font.BOLD,20);
         jf.setLayout(null);
+
+        l5 = new JLabel("Login Here");
+        l5.setFont(new Font("Times New Roman",Font.BOLD,30));
+        l5.setBounds(300,100,300,40);
+        jf.add(l5);
+
+        l1 = new JLabel("User Name : ");
+        l1.setFont(f);
+        l1.setBounds(200,250,200,25);
+        jf.add(l1);
+
+        t1 = new JTextField(20);
+        t1.setBounds(350,250,200,25);
+        t1.setToolTipText("Enter Username");
+        jf.add(t1);
+
+        l2 = new JLabel("Password  : ");
+        l2.setFont(f);
+        l2.setBounds(200,300,200,25);
+        jf.add(l2);
+
+        p1 = new JPasswordField(20);
+        p1.setBounds(350,300,200,25);
+        p1.setToolTipText("Enter Password");
+        jf.add(p1);
 
         jf.setTitle("Login");
         jf.setLocation(20,20);
