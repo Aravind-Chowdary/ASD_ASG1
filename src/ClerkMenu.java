19da94186b63a3fd13483f8d837d0eec821456d3
Aravind <aravindmullapudi@gmail.com>
2020-04-30 19:00:25 +0100

    Menu page after successful login

---
+++ src/ClerkMenu.java
@@ -0,0 +1,18 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+public class ClerkMenu extends JFrame implements ActionListener
+{
+    public ClerkMenu()
+    {
+    }
+
+    public void actionPerformed(ActionEvent ae)
+    {
+    }
+
+    public static void main(String args[])
+    {
+        new ClerkMenu();
+    }
+}
