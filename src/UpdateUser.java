c5d417a9cc8839d131e0fa96515ca7ec8bd840fe
Aravind <aravindmullapudi@gmail.com>
2020-05-01 00:27:28 +0100

    Labels and text fields

--- src/UpdateUser.java
+++ src/UpdateUser.java
@@ -6,12 +6,22 @@ import java.awt.event.ActionListener;
 public class UpdateUser extends JFrame implements ActionListener
 {
     JFrame jf;
+    JLabel l1;
+    JTextField t1;
     UpdateUser(){
         jf = new JFrame();
         jf.setLayout(null);
 
+        l1= new JLabel("User id *");
+        l1.setBounds(150,120,130,25);
+        jf.add(l1);
+
+        t1=new JTextField(20);
+        t1.setBounds(320,120,250,25);
+        t1.setToolTipText("Enter User id");
+        jf.add(t1);
+
         jf.setTitle("Update Users");
-        // jf.setSize(900,700);
         jf.setLocation(20,20);
         jf.setResizable(false);
         jf.getContentPane().setBackground(Color.cyan);
@@ -19,8 +29,8 @@ public class UpdateUser extends JFrame implements ActionListener
         jf.setBounds(0,0,screenSize.width, screenSize.height-50);
         jf.setVisible(true);
     }
-
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent ae)
+    {
 
     }
     public static void main(String args[])
@@ -28,3 +38,4 @@ public class UpdateUser extends JFrame implements ActionListener
         new UpdateUser();
     }
 }
+
