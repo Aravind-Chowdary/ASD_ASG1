7b81ce7bc249103a2471ba9b504041e02d69ba0a
Aravind <aravindmullapudi@gmail.com>
2020-04-30 22:45:43 +0100

    Details fields

--- src/AddNewBooking.java
+++ src/AddNewBooking.java
@@ -6,9 +6,25 @@ import java.awt.event.ActionListener;
 public class AddNewBooking extends JFrame implements ActionListener
 {
     JFrame jf;
+    JLabel l1,l2;
+    JTextField t2;
     AddNewBooking(){
         jf = new JFrame();
         jf.setLayout(null);
+
+        l1=new JLabel("Add New Booking");
+        l1.setFont(new Font("Times New Roman",Font.BOLD,25));
+        l1.setBounds(250,50,300,40);
+        l1.setForeground(Color.blue);
+        jf.add(l1);
+
+        l2 = new JLabel("Full Name*");
+        l2.setBounds(150,160,170,25);
+        jf.add(l2);
+
+        t2=new JTextField(20);
+        t2.setBounds(320,160,250,25);t2.setToolTipText("Enter Full Name");
+        jf.add(t2);
 
         jf.setTitle("ADD NEW BOOKING");
         jf.setLocation(20,20);
