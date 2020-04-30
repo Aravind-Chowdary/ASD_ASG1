775b511a4ab38638b2e8d84e3d5688cfa4857b7f
Aravind <aravindmullapudi@gmail.com>
2020-04-30 01:11:59 +0100

    Labels and text fields

--- src/UpdateRoomAvailability.java
+++ src/UpdateRoomAvailability.java
@@ -2,16 +2,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class UpdateRoomAvailability extends JFrame implements ActionListener
 {
     JFrame jf;
-    JLabel l1;
+    JLabel l1,l2;
     JTextField t1;
+    JComboBox c1;
+    DB db= null;
+    Connection con;
     UpdateRoomAvailability(){
         jf = new JFrame();
         jf.setLayout(null);
-
         l1 = new JLabel("Assign Id *");
         l1.setBounds(150,90,130,25);
         jf.add(l1);
@@ -21,7 +26,29 @@ public class UpdateRoomAvailability extends JFrame implements ActionListener
         t1.setToolTipText("Id");
         jf.add(t1);
 
+        l2 = new JLabel("Select Room *");
+        l2.setBounds(150,120,130,25);
+        jf.add(l2);
 
+        c1=new JComboBox();
+        c1.setBounds(320,120,200,25);c1.setToolTipText("Select Room");
+        jf.add(c1);
+        c1.addItem("Select Room");
+        try {
+            con=db.getConnection();
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("Select rtitle from room_manager");
+            while (rs.next()) {
+                String round = rs.getString("rtitle");
+                c1.addItem(round);
+            }
+            rs.close();
+            con.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
         jf.setTitle("Manage Bookings ");
         jf.setLocation(20,20);
         jf.setResizable(false);
