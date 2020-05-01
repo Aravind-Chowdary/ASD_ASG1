4fb4eecfaaa9faa58440a8205796aab580f86ba3
Aravind <aravindmullapudi@gmail.com>
2020-05-01 21:16:48 +0100

    Removed the fields

--- src/UpdateRoom.java
+++ src/UpdateRoom.java
@@ -260,7 +260,7 @@ public class UpdateRoom extends JFrame implements ActionListener
                 stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
                 rs = stmt.executeQuery("SELECT * from room_manager");
                 while (rs.next()) {
-                    model.insertRow(r++, new Object[]{rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7)});
+                    model.insertRow(r++, new Object[]{rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5)});
                 }
                 con.close();
             } catch (SQLException se) {
