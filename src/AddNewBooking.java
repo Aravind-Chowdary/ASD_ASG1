1fe8ad74aa77251bed936d9689adf8287be8f49b
Aravind <aravindmullapudi@gmail.com>
2020-05-01 04:54:07 +0100

    To view the data in the table same as in the other files

--- src/AddNewBooking.java
+++ src/AddNewBooking.java
@@ -27,6 +27,8 @@ public class AddNewBooking extends JFrame implements ActionListener
     JTable tabGrid = new JTable(model);
     JScrollPane scrlPane = new JScrollPane(tabGrid);
     PreparedStatement ps;
+    Statement stmt;
+    ResultSet rs;
     AddNewBooking(){
         jf = new JFrame();
         jf.setLayout(null);
@@ -258,7 +260,37 @@ public class AddNewBooking extends JFrame implements ActionListener
         {
             new AssignList();
         }
-
+        else if(ae.getSource()==b2)
+        {
+            if (model.getRowCount() > 0) {
+                for (int i = model.getRowCount() - 1; i > -1; i--) {
+                    model.removeRow(i);
+                }
+            }
+            int r = 0;
+            try
+            {
+                con=db.getConnection();
+                System.out.println("Connected to database.");
+                stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+                rs = stmt.executeQuery("select * from room_booking " );
+                while(rs.next())
+                {
+                    model.insertRow(r++, new Object[]{rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7),rs.getString(8),rs.getString(9) });
+                }
+                con.close();
+            }
+            catch(SQLException se)
+            {
+                System.out.println(se);
+                JOptionPane.showMessageDialog(null,"SQL Error:"+se);
+            }
+            catch(Exception e)
+            {
+                System.out.println(e);
+                JOptionPane.showMessageDialog(null,"Error:"+e);
+            }
+        }
     }
     public  static void main(String args[]){
 
