4358f13a6746d2c3a7ba5bad0746013cd09fc1b1
Aravind <aravindmullapudi@gmail.com>
2020-05-01 17:48:37 +0100

    Actions to the  clear and all buttons!!!

--- src/UpdateRoomBooking.java
+++ src/UpdateRoomBooking.java
@@ -341,7 +341,46 @@ public class UpdateRoomBooking extends JFrame implements ActionListener
                 }
             }
         }
-
+        else if(ae.getSource()==b2)
+        {//clear
+            t1.setText("");
+            t2.setText("");
+            t3.setText("");
+            t4.setText("");
+            t5.setText("");
+            t7.setText("");
+        }
+        else if(ae.getSource()==b3)
+        {//list
+            if (model.getRowCount() > 0) {
+                for (int i = model.getRowCount() - 1; i > -1; i--) {
+                    model.removeRow(i);
+                }
+            }
+            int r = 0;
+            try
+            {
+                con=db.getConnection();
+                System.out.println("Connected to database.");
+                stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+                rs = stmt.executeQuery("SELECT * from room_booking" );
+                while(rs.next())
+                {
+                    model.insertRow(r++, new Object[]{rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7),rs.getString(8),rs.getString(9) });
+                }
+                con.close();
+            }
+            catch(SQLException se)
+            {
+                System.out.println(se);
+                JOptionPane.showMessageDialog(null,"SQL Error"+se);
+            }
+            catch(Exception e)
+            {
+                System.out.println(e);
+                JOptionPane.showMessageDialog(null,"Error:"+e);
+            }
+        }
     }
 
 
